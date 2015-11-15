68a59517e1a7f2c412906ee26f7d46a965bad35e
Josh Sommmer <theoriginaljosh@gmail.com>
2015-11-15 11:37:21 -0500

    Front End development in place.
    added controller and services for front end web app.

--- Scripts/controllers/product-controller.js
+++ Scripts/controllers/product-controller.js
@@ -1,7 +1,42 @@
 ﻿angular.module("ShopKata", [])
-.controller("ProductsCntrl", ["$scope", function ($scope) {
+.controller("ProductsCntrl", ["$scope", "ApiDataService", function ($scope, ApiDataService) {
+    $scope.products = [];
+    $scope.availableQty = [0, 1, 2, 3, 4, 5, 6];
+    $scope.totalCost = 0;
+    $scope.cartItem = 0;
 
-}])
-.controller("CartCntrl", ["$scope", function ($scope) {
+    ApiDataService.get()
+       .success(function (data) {
+           $scope.products = data;
+           $scope.products.forEach(function (product, index) {
+               product.cartItem = $scope.availableQty[0];
+           });
+       });
 
+    $scope.cartItems = [];
+
+
+    $scope.addItem = function (producutId, qty) {
+        var updated = false;
+        //check to see if item already is in cart.
+        $.each($scope.cartItems, function () {
+            //updated item qty if it already exists
+            if (this.productId == producutId) {
+                this.qty = qty;
+                updated = true;
+            }
+        });
+        //if we didn't update a cart item. add it to the array.
+        if(!updated)
+            $scope.cartItems.push({
+                productId: producutId,
+                qty: qty
+            });
+
+        //get new totals
+        ApiDataService.getTotal($scope.cartItems)
+        .success(function (data) {
+            $scope.totalCost = data;
+        });
+    };
 }]);
---
+++ Scripts/services/data-service.js
@@ -0,0 +1,12 @@
+﻿angular.module("ShopKata")
+.service("ApiDataService", ["$http", function ($http) {
+    var baseUrl = "/api/Product";
+    
+    this.get = function () {
+        return $http.get(baseUrl);
+    }
+
+    this.getTotal = function(cartItems){
+        return $http.post(baseUrl + "/GetTotal", cartItems);
+    }
+}]);
